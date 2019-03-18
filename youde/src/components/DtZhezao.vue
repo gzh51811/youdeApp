cb59caa3dbd07f11f25cae787bd99f87e046e431
marry <1640004122@qq.com>
2019-03-18 14:33:38 +0800

    详情页静态

---
+++ youde/src/components/DtZhezao.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="all" ref="all">
+
+        <div class="ztop" ref='ztop'></div>
+        <div class="zbottom" ref='zbottom' @click.stop='close' >{{contents}}</div>
+
+    </div>
+</template>
+<script>
+export default {
+    
+    props : ['contents'],
+
+    methods : {
+        close(){
+            setTimeout(() => {
+                this.$refs.all.style.top = '100%';
+            }, 500);
+        }
+    },
+
+    mounted(){
+        this.$refs.all.style.height = innerHeight + "px";
+        this.$refs.ztop.style.height = innerHeight/2 + "px";
+        this.$refs.zbottom.style.height = innerHeight/2 + "px";
+
+        //阻止拖动
+        this.$refs.all.ontouchmove = function(e){
+            e.preventDefault();
+        }
+        
+
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+div.all{
+    width: 100%;
+    position: fixed;
+    top: 100%;
+    z-index:9999;
+    div.ztop{
+        background: rgb(79, 79, 218);
+        opacity: .2;
+    }
+
+
+    div.zbottom{
+        position: relative;
+        background: white;
+        z-index: 9999;
+    }
+}
+
+</style>
+
--- youde/src/main.js
+++ youde/src/main.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 
+
 import store from './configs/store'
 import router from './configs/router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import axios from 'axios';
+Vue.prototype.$axios = axios;
+
+
+
 
 // 引入全局样式
 import './stylesheets/index.css'
 
+
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
